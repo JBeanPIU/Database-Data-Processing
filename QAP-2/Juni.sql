abf44ba994ede492cdb240a9302685dc33d88f21
Cameron Beanland <cameronbeanland@hotmail.com>
2024-10-17 23:35:45 -0230

    Update Juni.sql
    -got rid of design notes, accomplished *most* of what was needed if not all
    -actually wrote code this time
    -fixed date to be correct, technically started on the 16th but only had the four empty tables

--- QAP-2/Juni.sql
+++ QAP-2/Juni.sql
@@ -1,45 +1,37 @@
 -- Program: Juni, a system managing university students, profs, courses, & all enrollments 
 -- By: Cameron Beanland.                                                                      
--- Date: October 16th, 2024.                      
-
-
--- very basic setup, and wouldn't even work properly in SQL lol, just a layout to get started and will be updated very shortly*
-
+-- Date: October 17th, 2024.                      
 
 -- Code was written using PostgreSQL, incase of any syntax errors prevalent. --
 
--- notes for later versions
--- A single student can register for multiple courses
--- A single professor can teach multiple courses
--- A course can only have one professor
--- A course can have multiple students
-
------ SECTION: DESIGN TABLES. -----
+----- SECTION: DESIGN TABLES, used to visualize database tables. -----
 CREATE TABLE students (
-student_id;
-first_name;
-last_name;
-email;
-school_enrollment_date;
+student_id BIGINT PRIMARY KEY, -- used for unique or specific values/numbers
+first_name VARCHAR(255) NOT NULL, -- varchar is used to store strings and 255 is the max amount of letters that can be used.
+last_name VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL,
+school_enrollment_date DATE NOT NULL, -- used to configure start date of program
 )
 
 CREATE TABLE professors (
-prof_id;
-first_name;
-last_name;
-department_name;
+prof_id BIGINT PRIMARY KEY, 
+first_name VARCHAR(255) NOT NULL,
+last_name VARCHAR(255) NOT NULL,
+department_name VARCHAR(255) NOT NULL,
 )
 
 CREATE TABLE courses (
-    course_id;
-    course_name;
-    course_description;
-    prof_id;
+    course_id BIGINT PRIMARY KEY,
+    course_name VARCHAR(255) NOT NULL,
+    course_description VARCHAR(255) NOT NULL,
+    prof_id BIGINT REFERENCES professors(prof_id), -- prof_id required for courses
 )
 
 CREATE TABLE enrollments (
-    student_id;
-    course_id;
-    enrollment_date;
-    -- make student_id and course_id a composite primary key* --
-)
+    student_id BIGINT, 
+    course_id BIGINT, 
+    enrollment_date DATE NOT NULL, 
+    PRIMARY KEY (student_id, course_id), 
+    FOREIGN KEY (student_id) REFERENCES students(student_id), -- Foreign key to students table
+    FOREIGN KEY (course_id) REFERENCES courses(course_id) -- Foreign key to courses table
+);
