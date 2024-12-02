539df5ce5c5e3ee5c776b316f4935afec8debc0e
Cameron Beanland <cameronbeanland@hotmail.com>
2024-12-02 18:24:32 -0330

    Update index.js
    Just added the Task const from Task.js, and setup the browser page setup so it doesn't display Cannot GET / (or whatever it says)

--- express-tasks-template/index.js
+++ express-tasks-template/index.js
@@ -2,9 +2,12 @@
 // By: Cameron Beanland
 // Date: Dec. 2nd, 2024
 
+// anything that has a /m in the comment is pre-existing code from the original git, but modified (self use to help figure out what i setup myself)
+
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const Task = require('./task'); // links back to the task.js file to nab Task const (i swear it's different from the task const below)
 
 app.use(express.json());
 
@@ -13,7 +16,12 @@ let tasks = [
     { id: 2, description: 'Read a book', status: 'complete' },
 ];
 
-// GET /tasks - Get all tasks
+// BROWSER page setup, anything that I need to display on a page will be put down here
+app.get('/', (request, response) => {
+    response.send('The J');
+});
+
+// GET /tasks - Get all tasks                           /m
 app.get('/tasks', (req, res) => {
     res.json(tasks);
 });
@@ -52,11 +60,6 @@ app.delete('/tasks/:id', (request, response) => {
         return response.status(404).json({ error: 'Task not found' });
     }
     response.json({ message: 'Task deleted successfully' });
-});
-
-// BROWSER page setup, anything that I need to display on a page will be put down here
-app.get('/', (request, response) => {
-    response.send('The J');
 });
 
 app.listen(PORT, () => {
